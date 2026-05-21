a54513b2a492a8e4945fbd590776b69d194423d6
soumikB <soumikbanerjee108@gmail.com>
2026-05-21 21:39:05 +0530

    Init db and table

---
+++ LendersTitle.Database/dbo/Tables/UserMaster.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[UserMaster] (
+    [AutoID]    INT            IDENTITY (1, 1) NOT NULL,
+    [UserName]  NVARCHAR (100) NOT NULL,
+    [FullName]  NVARCHAR (255) NULL,
+    [EmailID]   NVARCHAR (100) NOT NULL,
+    [PriPhnNo]  NVARCHAR (20)  NOT NULL,
+    [IsActive]  BIT            NULL,
+    [IsDefault] BIT            NULL,
+    PRIMARY KEY CLUSTERED ([UserName] ASC)
+);
+
