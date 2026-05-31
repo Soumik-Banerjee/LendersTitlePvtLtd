e79ba5f0092583978b989eb46403faef438ebf06
sayeljana <sayeljana05@gmail.com>
2026-06-01 00:44:09 +0530

    check in page UI only - implement

---
+++ LendersTitle.UI/Controllers/AttendanceCheckInController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LendersTitle.UI.Controllers
+{
+    public class AttendanceCheckInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Shared/AttendanceCheckIn/Index.cshtml");
+        }
+    }
+}
